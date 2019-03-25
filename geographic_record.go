041a350546d5016e79f025c6930334e72c07e555
Dustin Oprea <myselfasunder@gmail.com>
2019-03-25 03:58:48 -0400

    geographic_record.go: Add relationships field. Add Encode(). Add Dump(). Add tests.
    - Correctly initialize comments and relationships in constructor.
      function.

--- geographic_record.go
+++ geographic_record.go
@@ -3,10 +3,16 @@ package geoindex
 import (
 	"fmt"
 	"path"
+	"reflect"
 	"time"
 
 	"github.com/randomingenuity/go-utility/geographic"
 )
+
+type GeographicRecordRelatedTo struct {
+	GeographicRecord *GeographicRecord
+	Relationship     string
+}
 
 // GeographicRecord describes a single bit of geographic information, whether
 // it's a actual geographic data entry or an image with geographic data. Note
@@ -21,10 +27,110 @@ type GeographicRecord struct {
 	S2CellId      uint64
 	SourceName    string
 	Metadata      interface{}
+	comments      []string
+	relatedTo     []GeographicRecordRelatedTo
 }
 
 func (gr GeographicRecord) String() string {
 	return fmt.Sprintf("GeographicRecord<F=[%s] LAT=[%.6f] LON=[%.6f] CELL=[%d]>", path.Base(gr.Filepath), gr.Latitude, gr.Longitude, gr.S2CellId)
+}
+
+func (gr *GeographicRecord) Equal(other *GeographicRecord) bool {
+	if other.Timestamp.Equal(gr.Timestamp) != true {
+		return false
+	} else if other.Filepath != gr.Filepath {
+		return false
+	} else if other.HasGeographic != gr.HasGeographic {
+		return false
+	} else if fmt.Sprintf("%.6f", other.Latitude) != fmt.Sprintf("%.6f", gr.Latitude) {
+		return false
+	} else if fmt.Sprintf("%.6f", other.Longitude) != fmt.Sprintf("%.6f", gr.Longitude) {
+		return false
+	} else if other.S2CellId != gr.S2CellId {
+		return false
+	} else if other.SourceName != gr.SourceName {
+		return false
+	} else if reflect.DeepEqual(other.Metadata, gr.Metadata) != true {
+		return false
+	} else if reflect.DeepEqual(other.comments, gr.comments) != true {
+		return false
+	}
+
+	return true
+}
+
+func (gr *GeographicRecord) Dump() {
+	fmt.Printf("GEOGRAPHIC RECORD\n")
+	fmt.Printf("=================\n")
+
+	fmt.Printf("Timestamp: [%s]\n", gr.Timestamp)
+	fmt.Printf("Filepath: [%s]\n", gr.Filepath)
+	fmt.Printf("HasGeographic: [%v]\n", gr.HasGeographic)
+	fmt.Printf("Latitude: (%.6f)\n", gr.Latitude)
+	fmt.Printf("Longitude: (%.6f)\n", gr.Longitude)
+	fmt.Printf("S2CellId: (%d)\n", gr.S2CellId)
+	fmt.Printf("SourceName: [%s]\n", gr.SourceName)
+	fmt.Printf("Metadata: %v\n", gr.Metadata)
+
+	fmt.Printf("\n")
+}
+
+func (gr *GeographicRecord) Encode() map[string]interface{} {
+	relationships := gr.Relationships()
+	encodedRelationships := make(map[string][]map[string]interface{})
+	for type_, grList := range relationships {
+		encodedGrList := make([]map[string]interface{}, len(grList))
+		for i, gr := range grList {
+			encodedGrList[i] = gr.Encode()
+		}
+
+		encodedRelationships[type_] = encodedGrList
+	}
+
+	return map[string]interface{}{
+		"timestamp":      gr.Timestamp,
+		"filepath":       gr.Filepath,
+		"has_geographic": gr.HasGeographic,
+		"latitude":       gr.Latitude,
+		"longitude":      gr.Longitude,
+		"s2_cell_id":     gr.S2CellId,
+		"source_name":    gr.SourceName,
+		"metadata":       gr.Metadata,
+		"comments":       gr.Comments(),
+		"relationships":  encodedRelationships,
+	}
+}
+
+func (gr *GeographicRecord) Comments() []string {
+	return gr.comments
+}
+
+func (gr *GeographicRecord) AddComment(comment string) {
+	gr.comments = append(gr.comments, comment)
+}
+
+func (gr *GeographicRecord) Relationships() map[string][]*GeographicRecord {
+	relationshipsMap := make(map[string][]*GeographicRecord)
+	for _, grrt := range gr.relatedTo {
+		if existing, found := relationshipsMap[grrt.Relationship]; found == true {
+			existing = append(existing, grrt.GeographicRecord)
+		} else {
+			relationshipsMap[grrt.Relationship] = []*GeographicRecord{
+				grrt.GeographicRecord,
+			}
+		}
+	}
+
+	return relationshipsMap
+}
+
+func (gr *GeographicRecord) AddRelated(relatedGr *GeographicRecord, relationship string) {
+	grrt := GeographicRecordRelatedTo{
+		GeographicRecord: relatedGr,
+		Relationship:     relationship,
+	}
+
+	gr.relatedTo = append(gr.relatedTo, grrt)
 }
 
 func NewGeographicRecord(sourceName string, filepath string, timestamp time.Time, hasGeographic bool, latitude float64, longitude float64, metadata interface{}) (gr *GeographicRecord) {
@@ -38,6 +144,8 @@ func NewGeographicRecord(sourceName string, filepath string, timestamp time.Time
 		Filepath:      filepath,
 		HasGeographic: hasGeographic,
 		Metadata:      metadata,
+		comments:      make([]string, 0),
+		relatedTo:     make([]GeographicRecordRelatedTo, 0),
 	}
 
 	if hasGeographic == true {
---
+++ geographic_record_test.go
@@ -0,0 +1,93 @@
+package geoindex
+
+import (
+    "fmt"
+    "reflect"
+    "testing"
+    "time"
+)
+
+func TestGeographicRecord_Encode(t *testing.T) {
+    now := time.Now()
+
+    gr1 := NewGeographicRecord("source-name", "file.name1", now, true, 12.34, 34.56, nil)
+
+    expected := map[string]interface{}{
+        "timestamp":      now,
+        "filepath":       "file.name1",
+        "has_geographic": true,
+        "latitude":       12.34,
+        "longitude":      34.56,
+        "s2_cell_id":     uint64(1610222620212933235),
+        "source_name":    "source-name",
+        "metadata":       make(map[string]interface{}),
+        "comments":       make([]string, 0),
+        "relationships":  make(map[string][]*GeographicRecord),
+    }
+
+    actual := gr1.Encode()
+
+    if reflect.DeepEqual(actual, expected) != true {
+        fmt.Printf("ACTUAL:\n")
+
+        for k, v := range actual {
+            fmt.Printf("[%s] = [%v]\n", k, v)
+        }
+
+        fmt.Printf("\n")
+
+        fmt.Printf("EXPECTED:\n")
+
+        for k, v := range expected {
+            fmt.Printf("[%s] = [%v]\n", k, v)
+        }
+
+        fmt.Printf("\n")
+
+        t.Fatalf("Encoding not correct.")
+    }
+}
+
+func TestGeographicRecord_Metadata(t *testing.T) {
+    metadata := map[string]interface{}{
+        "aa": 123,
+    }
+
+    gr1 := NewGeographicRecord("source-name", "file.name1", time.Now(), true, 12.34, 34.56, metadata)
+
+    if reflect.DeepEqual(metadata, gr1.Metadata) != true {
+        t.Fatalf("Metadata not correct.")
+    }
+}
+
+func TestGeographicRecord_AddComment(t *testing.T) {
+    gr1 := NewGeographicRecord("source-name", "file.name1", time.Now(), true, 12.34, 34.56, nil)
+    gr1.AddComment("comment1")
+
+    expected := []string{
+        "comment1",
+    }
+
+    actual := gr1.Comments()
+
+    if reflect.DeepEqual(actual, expected) != true {
+        t.Fatalf("Comments not correct.")
+    }
+}
+
+func TestGeographicRecord_AddRelated(t *testing.T) {
+    gr1 := NewGeographicRecord("source-name", "file.name1", time.Now(), true, 12.34, 34.56, nil)
+
+    gr2 := NewGeographicRecord("source-name2", "file.name2", time.Now(), true, 12.34, 34.56, nil)
+    gr1.AddRelated(gr2, "estranged")
+
+    expected := map[string][]*GeographicRecord{
+        "estranged": []*GeographicRecord{gr2},
+    }
+
+    actual := gr1.Relationships()
+
+    if reflect.DeepEqual(actual, expected) != true {
+        t.Fatalf("Relationships not correct.")
+    }
+}
